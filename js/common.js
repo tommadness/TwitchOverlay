35d44adb3d8510fe132a8519752f10eee7013cb6
Thomas Cover <tom.m.cover@gmail.com>
2022-07-17 18:47:24 -0400

    Add emote functionality

--- js/common.js
+++ js/common.js
@@ -15,3 +15,47 @@ function hexToRgb(hex) {
 
     return (brightness > 125) ? 'black' : 'white';
   }
+
+  function sanitize(string){
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        "/": '&#x2F;',
+    };
+    const reg = /[&<>"'/]/ig;
+    return string.replace(reg, (match)=>(map[match]));
+  }
+
+  function replaceEmotes(message, emoteArray){
+    if (emoteArray == null){
+        return message;
+    }
+    const stringReplacements = [];
+
+    Object.entries(emoteArray).forEach(([id, positions]) => {
+        const position = positions[0];
+        const [start,end] = position.split("-");
+        const stringToReplace = message.substring(
+            parseInt(start, 10),
+            parseInt(end, 10) + 1
+        );
+
+        stringReplacements.push({
+            stringToReplace: stringToReplace,
+            replacement: `<img class="emote" src="https://static-cdn.jtvnw.net/emoticons/v1/${id}/3.0">`
+        })
+    })
+
+    const messageHTML = stringReplacements.reduce(
+        (acc, { stringToReplace, replacement }) => {
+          // obs browser doesn't seam to know about replaceAll
+          return acc.split(stringToReplace).join(replacement);
+        },
+        message
+      );
+    
+      return messageHTML;
+  }
